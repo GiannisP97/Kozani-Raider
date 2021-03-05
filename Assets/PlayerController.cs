7ede25e665138be0a423b387b261461af392743f
GiannisP97 <jpolymenidis3@gmail.com>
2021-03-05 21:06:38 +0200

    Basis level disign

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;   
+    public float jumpHeight;
+    public characterController2D characterController2D;
+
+    private Rigidbody2D rb2d;  
+    private bool jump = false;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("Space Pressed");
+            jump = true;
+        }
+
+        
+
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        Input.GetButtonDown("Jump");
+
+
+        float Horizontal_movement = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+
+        rb2d.velocity = new Vector2(Horizontal_movement,0f);
+        
+        if(jump){
+            rb2d.AddForce(new Vector2(0f,jumpHeight));
+            jump = false;
+        }
+       
+    }
+
+
+}
+
---
+++ Assets/characterController2D.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class characterController2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+	[SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
+	[SerializeField] private float speed = 10f;
+	[SerializeField] private float speed_while_air = 10f;
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
+	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
+	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
+	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
+	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
+
+	
+	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	private bool m_Grounded;            // Whether or not the player is grounded.
+	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+	private Rigidbody2D m_Rigidbody2D;
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	private Vector3 m_Velocity = Vector3.zero;
+
+	private bool jump = false;
+
+	[Header("Events")]
+	[Space]
+
+	public UnityEvent OnLandEvent;
+
+	[System.Serializable]
+	public class BoolEvent : UnityEvent<bool> { }
+
+	public BoolEvent OnCrouchEvent;
+	private bool m_wasCrouching = false;
+
+	private void Awake()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+		if (OnLandEvent == null)
+			OnLandEvent = new UnityEvent();
+
+		if (OnCrouchEvent == null)
+			OnCrouchEvent = new BoolEvent();
+	}
+	void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            //Debug.Log("Space Pressed");
+            jump = true;
+        }
+	}
+
+	private void FixedUpdate()
+	{
+		bool wasGrounded = m_Grounded;
+		m_Grounded = false;
+	
+		float Horizontal_movement = Input.GetAxis("Horizontal") * Time.fixedDeltaTime;
+
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				m_Grounded = true;
+				if (!wasGrounded)
+					OnLandEvent.Invoke();
+			}
+		}
+		//Debug.Log(m_Grounded);
+		Move(Horizontal_movement,false,jump);
+		jump = false;
+	}
+
+
+	public void Move(float move, bool crouch, bool jump)
+	{
+		//only control the player if grounded or airControl is turned on
+		if (m_Grounded || m_AirControl)
+		{
+			Vector3 targetVelocity;
+			// Move the character by finding the target velocity
+			if(!m_Grounded){
+				 targetVelocity = new Vector2(move * 10f * speed_while_air, m_Rigidbody2D.velocity.y);
+			}
+			else{
+				targetVelocity = new Vector2(move * 10f * speed, m_Rigidbody2D.velocity.y);
+			}
+				
+			// And then smoothing it out and applying it to the character
+			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+			// If the input is moving the player right and the player is facing left...
+			if (move > 0 && !m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+			// Otherwise if the input is moving the player left and the player is facing right...
+			else if (move < 0 && m_FacingRight)
+			{
+				// ... flip the player.
+				Flip();
+			}
+		}
+		// If the player should jump...
+		if (m_Grounded && jump)
+		{
+			// Add a vertical force to the player.
+			m_Grounded = false;
+			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+		}
+	}
+
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+
+	private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log(other.gameObject.name);
+        if(other.GetComponent<startgame>()!=null){
+            this.GetComponent<AutoScroller>().scrolling = true;
+        }
+    }
+}
