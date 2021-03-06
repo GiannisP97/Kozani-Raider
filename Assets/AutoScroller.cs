533d9c42d906d3a39709f32bb3b7d70163224282
GiannisP97 <jpolymenidis3@gmail.com>
2021-03-06 15:49:32 +0200

    level disign

--- Assets/AutoScroller.cs
+++ Assets/AutoScroller.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoScroller : MonoBehaviour
-{
+{  
+    public float speed = 1;
+    public float fixing_speed_y = 1;
     public Transform camera;
     public bool scrolling = false;
     [SerializeField] private float ScrollSpeed;
@@ -16,7 +18,11 @@ public class AutoScroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float dist = this.transform.position.y - camera.position.y;
+        if(Mathf.Abs(dist)>1){
+            camera.position = camera.position + new Vector3(0,dist*Time.deltaTime* fixing_speed_y,0);
+        }
         if(scrolling)
-            camera.transform.position = camera.position + new Vector3(ScrollSpeed*Time.deltaTime,0,0);
+            camera.transform.position = camera.position + new Vector3(ScrollSpeed*speed*Time.deltaTime,0,0);
     }
 }
