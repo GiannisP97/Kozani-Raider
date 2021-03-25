79ad0f21d3250836aa4ba25f5bf6a3deb7844533
st1217 <ece01217@uowm.gr>
2021-03-25 13:09:43 +0200

    ui manager for questions
    πρόσθεσα στο ήδη υπάρχον script UiManager σχετικά με την εμφάνιση των ερωτήσεων μετά την επιλογή

--- Assets/UiManager.cs
+++ Assets/UiManager.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
     public GameObject[] hearts;
+
+    [SerializeField]
+    private Text questionText;
+    [SerializeField]
+    private Button[] answerButtons;
+
+    [SerializeField]
+    private GameObject correctAnswerPopup;
+    [SerializeField]
+    private GameObject wrongAnswerPopup;
+
 
 
     // Start is called before the first frame update
@@ -18,4 +30,38 @@ public class UiManager : MonoBehaviour
     {
         
     }
+
+    public void QuestionUISetup(question q)
+    {
+
+        correctAnswerPopup.SetActive(false);
+        wrongAnswerPopup.SetActive(false);
+        questionText.text = q.QuestionText;
+
+        for(int i=0;i<q.Answers.Length;i++)
+        {
+            answerButtons[i].GetComponentInChildren<Text>().text=q.Answers[i];
+            answerButtons[i].gameObject.SetActive(true);
+        }
+
+    }
+
+    public void rightOrWrongPopup(bool a)
+    {
+        for(int i=0;i<3;i++)
+        {
+            answerButtons[i].gameObject.SetActive(false);
+        }
+        if(a)
+        {
+            correctAnswerPopup.SetActive(true);
+        }
+        else
+        {
+            wrongAnswerPopup.SetActive(true);
+        }
+    }
+
+
+
 }
