ea6e2a7f287eb75facc76dc0c6ccbe4b2cca8652
GiannisP97 <jpolymenidis3@gmail.com>
2021-03-26 21:04:59 +0200

    fdsf

---
+++ Assets/Resources/Questions/question.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenuAttribute]
+public class question : ScriptableObject
+{
+    [SerializeField]
+    private string questiontext;
+
+    [SerializeField]
+    private string[] answers;
+
+    [SerializeField]
+    private int correct;
+
+    public string QuestionText{get{return questiontext;}}
+    public string[] Answers{get{return answers;}}
+    public int CorrectAnswer{get{return correct;}}
+
+    public bool Asked{get; internal set;}
+
+
+
+
+}
---
+++ Assets/questionSelection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class questionSelection : MonoBehaviour
+{
+    private question[] allQuestions;
+
+    private void Awake()
+    {
+        LoadQuestions();
+    }
+
+    private void LoadQuestions()
+    {
+        allQuestions = Resources.LoadAll<question>("Questions");
+    }
+
+    public question SelectQuestion()
+    {
+        ResetIfAllAsked();
+
+        question q = allQuestions.Where(t => t.Asked == false).OrderBy(t => UnityEngine.Random.Range(0, int.MaxValue)).FirstOrDefault();
+
+        q.Asked = true;
+        return q;
+
+    }
+
+    private void ResetIfAllAsked()
+    {
+        if(allQuestions.Any(t => t.Asked == false) == false)
+        {
+            foreach(question q in allQuestions)
+            {
+                q.Asked = false;
+            }
+        }
+
+
+    }
+
+}
